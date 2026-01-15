14b2b57e816ba7d6a227485faa659883ab4f05e7
Waermepur <info@xn--wrmepur-5wa.com>
2026-01-15 16:12:48 +0100

    Refactor root element handling in index.tsx

--- index.tsx
+++ index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = document.getElementById('root');
-
-if (!root) {
-  throw new Error('Root element not found');
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
 }
 
-ReactDOM.createRoot(root).render(
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
