246f310510a5a9ba3b14fbb00df92504867b1660
Waermepur <info@xn--wrmepur-5wa.com>
2026-01-15 17:43:01 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- DIESER PUNKT IST DER RETTER!
+  base: './', // 👈 WICHTIG: Der Punkt sorgt dafür, dass es ÜBERALL läuft
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
