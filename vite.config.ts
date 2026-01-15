99a0f5de038b5b055a9d8125e85c0d79e56fdf78
Waermepur <info@xn--wrmepur-5wa.com>
2026-01-15 17:33:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: './', // <--- DIESER PUNKT IST DER RETTER!
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true,
   }
 });
